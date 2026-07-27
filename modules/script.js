9af4c386744dda2b8cfa3f2de89b628f20609f1a
SagarDevop <sagar.singh44818@gmail.com>
2026-07-27 10:33:27 +0530

    Add comprehensive function examples and explanations to script.js

--- modules/script.js
+++ modules/script.js
@@ -315,3 +315,247 @@
 //     console.log(i);
 // }
 
+// -------------------------------------->
+
+// FUNCTIONS
+
+// way of making a function 
+
+//1
+
+// function declaration
+
+// function functionName(parameters){
+//     code to be executed
+// }
+
+// calling a function
+
+// functionName(arguments);
+
+// function hello(){
+//     console.log("hello world");
+// }
+
+// hello();
+
+// 2
+// function expression 
+
+// here varibale name is a function name 
+
+// let variableName = function(parameters){
+//     // code to be executed
+// }
+
+// let fnc = function(){
+//     console.log("hello world");
+// }
+// fnc();
+
+// 3
+// fat arrow function
+
+// let fnc = () => {
+//     console.log("hello world");
+// }
+// fnc();
+
+// function with parameters
+
+// now we can send parameters in function as much parameters we want to send we can
+
+// function functionName(parameters){
+//     code to be executed
+// }
+// functionName(parameters)
+
+// function run(animal){
+//     console.log(`${animal} is running`);
+// }
+
+// run("Dog");
+// run("Cat");
+
+
+// function add(a, b){
+//     console.log(`sum of ${a} and ${b} is ${a+b} `)
+// }
+
+// add(4,5)
+
+// default parameters ----------->
+
+// ok so default parameter work when on calling function we not provide 
+// arguments so we can give default parameter 
+// as you se v1 = 1     and  v2 = 2 is default parameters
+
+// function add (v1 = 1, v2 = 2){
+//     console.log(v1 + v2)
+
+// }
+// add();
+
+// rest parameter ---------->
+
+//jab arguments bahut sare ho to tumein utne hi parameters 
+// banane padenge, issey bachne ke liye hum rest ka use krte hai 
+// agr ... function ke parameter space mein laga to wo rest operator hai 
+
+// function adcd(...val){
+//     console.log(val);
+// }
+
+// adcd(1,2,3,4,5,6);
+
+// function adcd(a,b,c,...val){
+//     console.log(a,b,c,val);
+// }
+
+// adcd(1,2,3,4,5,6);
+
+// return value
+// function can return value
+
+// function fnc(){
+//     return 21;
+// }
+
+// let a = fnc()
+// console.log(a)
+
+// function fnc(v){
+//     return 21 + v;
+// }
+
+// let a = fnc(34)
+// console.log(a)
+
+// First class function --------------->
+
+// in js function ko hum value ki trh treat kr sakte hai 
+
+// function ab(val){
+//     val(23);
+// }
+
+// ab(function(a){
+//     console.log(`${a} this is something crazy `)
+// })
+
+// here we are sending function in argument as value the only thing we need to know is we can not name function
+// as its name is prameter in upper function
+
+// high order function =------>
+
+// vo function hota hai jo ki return kare function ya fir accept kare ek  fnc apne parameter mein
+
+// function abcd( hof){     // this is hof 
+//     hof()
+// }
+
+// abcd(function(){
+//     console.log(" i am not hof the fuction accepting me in his parameter is ")
+// })
+
+// function abcd(){
+//     return function(){
+//         console.log("returning the fuction so abcd is hof")
+//     }
+// }
+
+
+// let as = abcd()() // we are using to bracket bcz one to call abcd and one to function inside it 
+// console.log(as)
+
+
+// pure vs impure functions ------------->
+
+// aisa fnc jo ki baahar ki value ko naa badle wo hai pure fnc 
+
+// let a = 12 ;
+
+// function as(){
+//     console.log("heh")
+// }
+
+// aisa fnc jo bhr kki value ko bdl de vo hai impure fnc 
+
+// function as(){
+//     a++
+// }
+
+// closures ---> ek fnc jo return kare ek aur function aur return hone walla fnc humesha use karega parent fnc ka koi variable 
+
+// function sagar(){
+//     let a = 12
+//     return function(){
+//         console.log(a)
+//     }
+// }
+
+// let me = sagar()() // two bracket bcz to call two function 
+// console.log(me)
+
+// lexical scoping-------->
+
+// function abcd() {
+//     let a = 123;
+//     function def(){
+//         let b = 13;
+//         function fed() {
+//             let c = 14;
+//         }
+//     }
+
+// }
+
+// so here we a can be use everywhere and b can be used unsder def and c can only use inside fed function
+
+// IIFE(immediately invoked function expression)------------>
+
+// (
+//     function(){
+//     console.log("turnt chale ga kyuki ye call ho chuka hai ")
+// }
+// )(); // this is iife 
+
+// hoisting in function ---->
+
+// in function statement and declaration we can use hosting like hum niche function banaye aur upr call kare to chalega 
+
+// asd();
+
+// function asd(){
+//     console.log("bana niceh pr uper bhi kam kr raha hai ")
+// }
+
+// asd(12);
+
+// function asd(a){
+//     console.log(` ${a} ana niceh pr uper bhi kam kr raha hai `)
+// }
+
+// --- pr hosting error so karegi if we do same thing using function expression 
+
+// a();
+
+// let a = function(){
+//     console.log("isme error ayega agr iske uper call kiya to")
+// }
+
+// practise by sherian 
+
+// function discountCal(discount){
+//     return function(price){
+//         return price - price * (discount/100)
+//     };
+// };
+
+// let ten = discountCal(10)
+// let twenty = discountCal(20)
+
+// console.log(ten(100));
+
+
+
